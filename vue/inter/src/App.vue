47e54cb161bfd290d97cdea97bc0e2b3be6d85ff
Alex Petrov <i@petrov.com.ru>
2019-02-07 11:06:10 +0300

    global & local components

--- vue/inter/src/App.vue
+++ vue/inter/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>{{ name }}</div>
+    <div>
+        <app-header></app-header>
+        <div>{{ name }}</div>
+        <compFooter/>
+    </div>
 </template>
 
 <script>
+    import compFooter from './Components/Header-footer/Footer.vue';
+
     export default {
         data() {
             return {
                 name: 'John',
             }
+        },
+        components: {
+            compFooter,
         },
     }
 </script>
--- vue/inter/src/main.js
+++ vue/inter/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Header from './Components/Header-footer/Header.vue';
+
+Vue.component('app-header', Header);
 
 new Vue({
     el: '#app',
